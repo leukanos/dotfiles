2bf4e214d74316553721ddc86955af022f6c1620
Łukasz Ostrowski <kontakt@lostrowski.pl>
2017-05-19 11:40:52 +0200

    Add brew install script

---
+++ README.md
@@ -0,0 +1,13 @@
+#  Leukanos' Dotfiles
+This is a collection of dotfiles and scripts I use for customizing OS X to my liking and setting up the software development tools I use on a day-to-day basis. 
+
+## Resources
+
+I actively watch the following repositories and add the best changes to this repository:
+- [GitHub ❤ ~/](http://dotfiles.github.com/)
+- [Mathias’s dotfiles](https://github.com/mathiasbynens/dotfiles)
+- [Nicolas Gallagher’s dotfiles](https://github.com/necolas/dotfiles)
+- [Cătălin’s dotfiles](https://github.com/alrra/dotfiles)
+- [Paul's dotfiles](https://github.com/paulirish/dotfiles)
+- [Jacob Gillespie’s dotfiles](https://github.com/jacobwg/dotfiles)
+- [Nick Plekhanov's Dotfiles](https://github.com/nicksp/dotfiles)
---
+++ install/brew.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Installs Homebrew and some of the common dependencies needed/desired for software development
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew tap homebrew/versions
+brew tap homebrew/dupes
+brew tap Goles/battery
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade --all
+
+apps=(
+  bash-completion2
+  coreutils
+  moreutils
+  findutils
+  git
+  git-extras
+  homebrew/completions/brew-cask-completion
+  homebrew/dupes/grep
+  homebrew/dupes/openssh
+  imagemagick --with-webp
+  tree
+  htop
+  wget
+  thefuck
+  rbenv
+  nvm
+  tig
+  yarn
+  libxml2
+  libxslt
+  jq
+  teamocil
+)
+
+brew install "${apps[@]}"
+
+# Remove outdated versions from the cellar
+brew cleanup
