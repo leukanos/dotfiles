97f8c03dc32e53c76deca153d647098d514ad2f0
leukanos <kontakt@lostrowski.pl>
2019-12-23 13:00:10 +0100

    Update

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -3,3 +3,25 @@
 # Update apt
 apt update
 apt dist-upgrade
+
+apps=(
+  awscli
+  bash-completion2
+  coreutils
+  curl
+  moreutils
+  findutils
+  git-extras
+  tree
+  htop
+  wget
+  thefuck
+  tig
+  libxml2
+  libxslt
+  jq
+  z
+  zsh-completions
+)
+
+apt install "${apps[@]}"
